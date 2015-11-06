9418106d0f40ae7068719c5aaeea313a3a5b35ac
Caleb Anderson <canderson@insidesales.com>
2015-11-05 22:47:09 -0700

    Added content

---
+++ c-Tree Date Converter/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace c_Tree_Date_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.tbDateToConvert = new System.Windows.Forms.TextBox();
+            this.lblConvertedDate = new System.Windows.Forms.Label();
+            this.btnConvertIt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.Location = new System.Drawing.Point(13, 13);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(259, 71);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = resources.GetString("lblInstructions.Text");
+            // 
+            // tbDateToConvert
+            // 
+            this.tbDateToConvert.Location = new System.Drawing.Point(88, 88);
+            this.tbDateToConvert.Name = "tbDateToConvert";
+            this.tbDateToConvert.Size = new System.Drawing.Size(100, 20);
+            this.tbDateToConvert.TabIndex = 1;
+            // 
+            // lblConvertedDate
+            // 
+            this.lblConvertedDate.BackColor = System.Drawing.Color.Silver;
+            this.lblConvertedDate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblConvertedDate.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConvertedDate.Location = new System.Drawing.Point(12, 141);
+            this.lblConvertedDate.Name = "lblConvertedDate";
+            this.lblConvertedDate.Size = new System.Drawing.Size(260, 82);
+            this.lblConvertedDate.TabIndex = 2;
+            this.lblConvertedDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnConvertIt
+            // 
+            this.btnConvertIt.Location = new System.Drawing.Point(99, 115);
+            this.btnConvertIt.Name = "btnConvertIt";
+            this.btnConvertIt.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertIt.TabIndex = 3;
+            this.btnConvertIt.Text = "Convert It!!";
+            this.btnConvertIt.UseVisualStyleBackColor = true;
+            this.btnConvertIt.Click += new System.EventHandler(this.btnConvertIt_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnConvertIt;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 234);
+            this.Controls.Add(this.btnConvertIt);
+            this.Controls.Add(this.lblConvertedDate);
+            this.Controls.Add(this.tbDateToConvert);
+            this.Controls.Add(this.lblInstructions);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 272);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 272);
+            this.Name = "Form1";
+            this.Text = "C-Tree Date Converter Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox tbDateToConvert;
+        private System.Windows.Forms.Label lblConvertedDate;
+        private System.Windows.Forms.Button btnConvertIt;
+    }
+}
+
---
+++ c-Tree Date Converter/Form1.cs
@@ -0,0 +1,42 @@
+﻿/* cTree Date Converter
+ * Author:              Caleb Anderson
+ * email address:       Caleb.Anderson@HenrySchein.com
+ * Description:         Some reports and utilities give dates as a large integer.  These dates are not easily 
+ *                      understood.  As such, this utility will take a date in the long integer format and return
+ *                      a date in a more conventional format.
+ * Initial Features:    
+ *          - Feature 1: Converts dates from numeric format to a more readable format.
+ */
+
+
+using System;
+using System.Windows.Forms;
+
+namespace c_Tree_Date_Converter
+{
+    public partial class Form1 : Form
+    {
+        DateTime _startDate = new DateTime(1700, 2, 28); //default start date for c-Tree
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertIt_Click(object sender, EventArgs e)
+        {
+            //The number can be larger than what can be stored as an int32.  To compensate, use the double data type.
+            double addDays;
+            if (double.TryParse(tbDateToConvert.Text.Trim(), out addDays))
+                //if the textbox parses, add the number of days listed in the TextBox to the default start date for c-Tree
+                lblConvertedDate.Text = _startDate.AddDays(addDays).ToShortDateString();
+            else
+                //if it doesn't parse, provide error message.
+                MessageBox.Show(
+                        "Please enter a valid number", 
+                        "Invalid C-Tree Date Format", 
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+        }
+    }
+}
